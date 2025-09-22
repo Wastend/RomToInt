f8077de232895cd0c4c3aae54dcef6ebbfa9320f
kovalenko_evgeniy <kovalenko@a2design.biz>
2025-09-22 17:29:08 +0600

    added reverse solution

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ const romanMap = [
   { value: 1, symbol: "I" },
 ];
 
+const romanMap2 = [
+  { value: 900, symbol: "CM" },
+  { value: 1000, symbol: "M" },
+  { value: 400, symbol: "CD" },
+  { value: 500, symbol: "D" },
+  { value: 90, symbol: "XC" },
+  { value: 100, symbol: "C" },
+  { value: 40, symbol: "XL" },
+  { value: 50, symbol: "L" },
+  { value: 9, symbol: "IX" },
+  { value: 10, symbol: "X" },
+  { value: 4, symbol: "IV" },
+  { value: 5, symbol: "V" },
+  { value: 1, symbol: "I" },
+];
+
 var intToRoman = function(num) {
   let result = "";
   for(const {value, symbol} of romanMap) {
@@ -25,4 +41,16 @@ var intToRoman = function(num) {
   return result
 };
 
+var romanToInt = function(str = "") {
+  let num = 0;
+  for(const {value, symbol} of romanMap2) {
+    while(str.includes(symbol)) {
+      str = str.replace(symbol, "");
+      num += value
+    }
+  }
+  return num
+};
+
 console.log(intToRoman(3749));
+console.log(romanToInt("III"));
